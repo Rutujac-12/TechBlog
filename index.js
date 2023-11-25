59d1c19c00f9b6f168a380ccdeffb7ac58fcd484
Chaudhari Rutuja Durgesh <142911369+Rutujac-12@users.noreply.github.com>
2023-11-25 23:57:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyparser=require("body-parser");
 const mongoose=require("mongoose");
 const blogPostArray = require("./data");
-require("dotenv").conflig();
+require("dotenv").config();
 const app = express();
 
 app.set('view engine', 'ejs');
